aa47676a623d6555752ab5ebe2b6537160e0e9c9
lee22br <leeiglezias@gmail.com>
2025-06-18 09:49:35 -0300

    Exercism Plane Ticket

--- python/plane-tickets/generators.py
+++ python/plane-tickets/generators.py
@@ -3,38 +3,33 @@
 
 def generate_seat_letters(number):
     """Generate a series of letters for airline seats.
-
     :param number: int - total number of seat letters to be generated.
     :return: generator - generator that yields seat letters.
-
     Seat letters are generated from A to D.
     After D it should start again with A.
-
     Example: A, B, C, D
-
     """
 
-    pass
+    letters = ('A', 'B', 'C', 'D')
+    for letter in range(number):
+        yield letters[letter % 4]
 
 
 def generate_seats(number):
     """Generate a series of identifiers for airline seats.
-
     :param number: int - total number of seats to be generated.
     :return: generator - generator that yields seat numbers.
-
     A seat number consists of the row number and the seat letter.
-
     There is no row 13.
     Each row has 4 seats.
-
     Seats should be sorted from low to high.
-
     Example: 3C, 3D, 4A, 4B
-
     """
-
-    pass
+    result = ''
+    number = number + 4 if number >= 13 else number
+    seat_letter = generate_seat_letters(number)
+    result = str(row_number) + next(seat_letter_generator) for seat in range(number)if (row_number := seat // 4 + 1) != 13
+    return result
 
 def assign_seats(passengers):
     """Assign seats to passengers.
