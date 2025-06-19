e9d916ab47cd9d0cdf92ac255e675f94d59dfe36
leandro.iglezias <leeiglezias@gmail.com>
2025-06-19 20:49:23 -0300

    Exercism Plane Tickets

--- python/plane-tickets/generators.py
+++ python/plane-tickets/generators.py
@@ -9,11 +9,9 @@ def generate_seat_letters(number):
     After D it should start again with A.
     Example: A, B, C, D
     """
-
     letters = ('A', 'B', 'C', 'D')
     for letter in range(number):
         yield letters[letter % 4]
-
 
 def generate_seats(number):
     """Generate a series of identifiers for airline seats.
@@ -28,28 +26,27 @@ def generate_seats(number):
     result = ''
     number = number + 4 if number >= 13 else number
     seat_letter = generate_seat_letters(number)
-    result = str(row_number) + next(seat_letter_generator) for seat in range(number)if (row_number := seat // 4 + 1) != 13
+    result = (str(row_number) + next(seat_letter_generator) for seat in range(number)if (row_number := seat // 4 + 1) != 13)
     return result
 
 def assign_seats(passengers):
     """Assign seats to passengers.
-
     :param passengers: list[str] - a list of strings containing names of passengers.
     :return: dict - with the names of the passengers as keys and seat numbers as values.
-
     Example output: {"Adele": "1A", "Björk": "1B"}
-
     """
-
-    pass
+    result = dict()
+    seat = generate_seats(len(passengers))
+    result = {name: next(seat) for name in passengers}
+    return result
 
 def generate_codes(seat_numbers, flight_id):
     """Generate codes for a ticket.
-
     :param seat_numbers: list[str] - list of seat numbers.
     :param flight_id: str - string containing the flight identifier.
     :return: generator - generator that yields 12 character long ticket codes.
-
     """
+    for number in seat_numbers:
+        code = number + flight_id
+        yield code + '0' * (12 - len(code))
 
-    pass
