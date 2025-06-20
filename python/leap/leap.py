4448d27488539858917475c3ae41579bb67f7e4e
lee22br <leeiglezias@gmail.com>
2025-06-20 16:16:18 -0300

    Exercism Leap (fixed)

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -2,9 +2,9 @@ def leap_year(year):
     result = False
     if year % 400 == 0:
         result = True
-    if year % 100 == 0:
+    elif year % 100 == 0:
         result = False
-    if year % 4 == 0:
+    elif year % 4 == 0:
         result = True
     return result
 
