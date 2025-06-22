001eba9e6630975e6c2c4d116ab33e65fee733ca
leandro.iglezias <leeiglezias@gmail.com>
2025-06-22 18:35:42 -0300

    Exercism Triangle Passed in all tests

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,13 +1,18 @@
 def equilateral(sides):
     a, b, c = sorted(sides)
-    return a == b and b == c and a == c
+    if a > 0 and b > 0 and c > 0:
+        return a == b and b == c and a == c
+    return False
 
 
 def isosceles(sides):
     a, b, c = sorted(sides)
-    return 0 < a and c < a + b and b in (a, c)
-
+    if a > 0 and b > 0 and c > 0:
+        return 0 < a and c < a + b and b in (a, c)
+    return False
 
 def scalene(sides):
     a, b, c = sorted(sides)
-    return a != b and b != c
+    if a > 0 and b > 0 and 0 < c < a + b:
+        return a != b and b != c
+    return False
