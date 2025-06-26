5f6a5e6e0ce3c24bd1ac8874ae82c64846a66fe2
leandro.iglezias <leeiglezias@gmail.com>
2025-06-25 22:22:59 -0300

    Exercism Zig Isogram OK

--- zig/isogram/isogram.zig
+++ zig/isogram/isogram.zig
@@ -1,8 +1,24 @@
+const std = @import("std");
+
 pub fn isIsogram(str: []const u8) bool {
+    const ascii_a = 97; // 'a'
+    const ascii_z = 122; // 'z'
+    const ascii_A = 65; // 'A'
+    const ascii_Z = 90; // 'Z'
+    var alphabetList = [_]bool{false} ** 26;
+
     for (str) |c| {
-        std.debug.print(c);
+        if (c >= ascii_a and c <= ascii_z or c >= ascii_A and c <= ascii_Z) {
+            const pos = std.ascii.toLower(c) - ascii_a;
+            if (alphabetList[pos] == false) {
+                alphabetList[pos] = true;
+            } else {
+                return false;
+            }
+        }
     }
+    return true;
 }
 pub fn main() !void {
-    isIsogram("abc");
+    std.debug.print("Is Isogram: {}", .{isIsogram("abx")});
 }
