9c9800f1b0e7f7ec6d158a83de4aff792bd11c4a
leandro.iglezias <leeiglezias@gmail.com>
2025-06-28 23:27:41 -0300

    Exercism nucleotide Count with IFs (fail return error)

--- zig/nucleotide-count/nucleotide_count.zig
+++ zig/nucleotide-count/nucleotide_count.zig
@@ -12,15 +12,17 @@ pub fn countNucleotides(s: []const u8) NucleotideError!Counts {
     const ascii_C = 67; // 'C
     const ascii_G = 71; // 'G'
     const ascii_T = 84; // 'T'
-    const dnaCount = Counts;
+    var dnaCount: Counts = .{
+        .a = 0,
+        .c = 0,
+        .g = 0,
+        .t = 0,
+    };
     for (s) |ch| {
-        switch (ch) {
-            ascii_A => dnaCount.a += 1,
-            ascii_C => dnaCount.c += 1,
-            ascii_G => dnaCount.g += 1,
-            ascii_T => dnaCount.t += 1,
-            else => 0, //('Invalid nucleotide in strand'),
-        }
+        if (ascii_A == ch) dnaCount.a += 1;
+        if (ascii_C == ch) dnaCount.c += 1;
+        if (ascii_G == ch) dnaCount.g += 1;
+        if (ascii_T == ch) dnaCount.t += 1;
     }
-    return Counts;
+    return dnaCount;
 }
