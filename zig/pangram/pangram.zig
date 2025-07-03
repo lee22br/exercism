3b4d38a530a826d6806cdc425fdbff2583334557
lee22br <leeiglezias@gmail.com>
2025-07-03 10:24:08 -0300

    Exercism Zig pangram Passed

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -1,3 +1,20 @@
+const std = @import ("std");
 pub fn isPangram(str: []const u8) bool {
+    const ascii_a = 97; // 'a'
+    var alphabetList = [_]bool{false} ** 26;
+    var count:u8 = 0;
 
+    if (str.len < 26) return false;
+
+    for (str) |c| {
+        if (std.ascii.isAlphabetic(c)){
+            const pos = std.ascii.toLower(c) - ascii_a;
+            if (alphabetList[pos] == false) {
+                alphabetList[pos] = true;
+                count += 1;
+            }
+        }
+    }
+    return count == 26;
 }
+
