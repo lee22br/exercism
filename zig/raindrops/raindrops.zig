f37f95a74c4901536c3b9cc6ae8c6ee538dd677a
leandro.iglezias <leeiglezias@gmail.com>
2025-07-05 18:47:42 -0300

    Exercism raindrops Zig tests failed

--- zig/raindrops/raindrops.zig
+++ zig/raindrops/raindrops.zig
@@ -1,4 +1,18 @@
-pub fn convert(buffer: []u8, n: u32) []const u8 {
+const std = @import("std");
 
-    @compileError("please implement the convert function");
+pub fn convert(buffer: []u8, n: u32) []const u8 {
+    var stream = std.io.fixedBufferStream(buffer);
+    const isPling = n % 3 == 0;
+    const isPlang = n % 5 == 0;
+    const isPlong = n % 7 == 0;
+
+    if (isPling) stream.writer().writeAll("Pling") catch unreachable;
+    if (isPlang) stream.writer().writeAll("Plang") catch unreachable;
+    if (isPlong) stream.writer().writeAll("Plong") catch unreachable;
+
+    if (!isPlang and !isPlang and !isPlong) {
+        stream.writer().print("{d}", .{n}) catch unreachable;
+    }
+
+    return stream.getWritten();
 }
