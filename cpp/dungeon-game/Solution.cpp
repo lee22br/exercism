34c97e32c120c202a2a31d2aa840d729c26c1cb6
leandro.iglezias <leeiglezias@gmail.com>
2025-07-16 16:57:59 -0300

    dungeon-game Solution 1

--- cpp/dungeon-game/Solution.cpp
+++ cpp/dungeon-game/Solution.cpp
@@ -1,52 +1,69 @@
+#include <iostream>
+#include <vector>
 
 class Solution {
-    int tabX, tabY;
-    int right, down;
+
 public:
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    static int calculateMinimumHP(std::vector<std::vector<int>>& dungeon) {
+        int tabX, tabY, lastPosX, lastPosY;
+        int right, down;
         tabX = dungeon.size();
         tabY = dungeon[0].size();
-        int lastPosX = tabX - 1;
-        int lastPosY = tabY - 1;
+        lastPosX = tabX - 1;
+        lastPosY = tabY - 1;
 
-        //store the matrix with min health
-        vector<vector<int>> pathMin(tabX,vector<int>(tabY,0));
+        // store the matrix with min health
+        std::vector<std::vector<int>> pathMin(tabX, std::vector<int>(tabY, 0));
 
-        for (int posX = lastPosX; posX >= 0; posX--){
-            for (int posY = lastPosY; posY >= 0; posY--){
+        for (int posX = lastPosX; posX >= 0; posX--) {
+            for (int posY = lastPosY; posY >= 0; posY--) {
+                // last position of table
                 if (posX == lastPosY && posY == lastPosY) {
-                    if (dungeon[posX][posY] < 0){
-                        pathMin[posX][posY] =  dungeon[posX][posY];
+                    if (dungeon[posX][posY] < 0) {
+                        pathMin[posX][posY] = dungeon[posX][posY];
                     } else {
                         pathMin[posX][posY] = 0;
                     }
                 } else if (posY == lastPosY) {
-                    if (dungeon[posX][posY] + pathMin[posX + 1][posY] < 0){
-                        pathMin[posX][posY] = dungeon[posX][posY] + pathMin[posX + 1][posY] ;
+                    if (dungeon[posX][posY] + pathMin[posX + 1][posY] < 0) {
+                        pathMin[posX][posY] =
+                            dungeon[posX][posY] + pathMin[posX + 1][posY];
                     } else {
                         pathMin[posX][posY] = 0;
                     }
                 } else if (posX == lastPosX) {
-                    if (dungeon[posX][posY] + pathMin[posX][posY + 1] < 0){
-                        pathMin[posX][posY] = dungeon[posX][posY] + pathMin[posX][posY + 1];
+                    if (dungeon[posX][posY] + pathMin[posX][posY + 1] < 0) {
+                        pathMin[posX][posY] =
+                            dungeon[posX][posY] + pathMin[posX][posY + 1];
                     } else {
                         pathMin[posX][posY] = 0;
                     }
                 } else {
-                    if (dungeon[posX][posY] + pathMin[posX][posY + 1]  < 0){
-                        right = dungeon[posX][posY] + pathMin[posX][posY + 1] ;
+                    if (dungeon[posX][posY] + pathMin[posX][posY + 1] < 0) {
+                        right = dungeon[posX][posY] + pathMin[posX][posY + 1];
                     } else {
                         right = 0;
                     }
-                    if (dungeon[posX][posY] + pathMin[posX + 1][posY]  < 0){
-                        down = dungeon[posX][posY] + pathMin[posX + 1 ][posY] ;
+                    if (dungeon[posX][posY] + pathMin[posX + 1][posY] < 0) {
+                        down = dungeon[posX][posY] + pathMin[posX + 1][posY];
                     } else {
                         down = 0;
                     }
-                    pathMin[posX][posY] = max(right, down);
+                    pathMin[posX][posY] = std::max(right, down);
                 }
             }
         }
-        return -pathMin[0][0] + 1;
+        return pathMin[0][0] * (-1) + 1;
     }
 };
+
+int main (){
+    std::vector<std::vector<int>> input = {{0}}; //expected 1
+    std::vector<std::vector<int>> input2 = {{-2,-3,3},{-5,-10,1},{10,30,-5}}; //expected 7
+
+    int result = Solution::calculateMinimumHP(input);
+
+    printf("Minimun Life is: %d", result);
+
+
+}
