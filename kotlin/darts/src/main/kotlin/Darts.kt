e8e3c382eed79304b3b957ab5c21fa9631a87cd4
lee22br <leeiglezias@gmail.com>
2025-07-30 08:47:01 -0300

    Exercism Kotlin darts All Test Passed

--- kotlin/darts/src/main/kotlin/Darts.kt
+++ kotlin/darts/src/main/kotlin/Darts.kt
@@ -1,6 +1,15 @@
+import java.lang.StrictMath.pow
+import kotlin.math.sqrt
+
+
 object Darts {
 
-    fun score(x: Any, y: Any /* choose proper types! */): Int {
-        TODO("Implement this function to complete the task")
+    fun score(x: Number, y: Number): Int {
+        val distance = sqrt(pow(x.toDouble(),2.0)  + pow(y.toDouble(), 2.0))
+        var result = 0
+        if (distance <= 1) result = 10
+        if (distance > 1 && distance <= 5) result = 5
+        if (distance > 5 && distance <= 10) result = 1
+        return result
     }
 }
