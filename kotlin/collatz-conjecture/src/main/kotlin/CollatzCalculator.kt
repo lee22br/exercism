e147057a5e05c9bb0ee1a9753e2e4b750ea6df95
lee22br <leeiglezias@gmail.com>
2025-07-30 08:16:13 -0300

    Exercism Kotlin Collatz conjecture All Tests Passed

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,5 +1,21 @@
 object CollatzCalculator {
     fun computeStepCount(start: Int): Int {
-        TODO("Implement the function to complete the task")
+        var actualNum = start
+        var steps: Int = 0
+        require(start > 0) {"Only positive natural numbers are allowed"}
+
+        while (actualNum > 1){
+            if (isEven(actualNum)){
+                actualNum = actualNum / 2;
+            } else {
+                actualNum = actualNum * 3 + 1;
+            }
+            steps ++;
+        }
+        return steps;
+    }
+
+    fun isEven(num: Int): Boolean {
+        return num % 2 == 0;
     }
 }
