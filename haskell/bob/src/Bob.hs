b56a84c866e1e3de8338d02225031b172d55efdb
leandro.iglezias <leeiglezias@gmail.com>
2025-08-11 21:51:57 -0300

    Exercism Haskell Bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,15 @@
 module Bob (responseFor) where
 
+import Data.Char
+import Data.List
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+    | all isSpace xs = "Fine. Be that way!"
+    | isQuestion = if isYelling then "Calm down, I know what I'm doing!"
+                                else "Sure."
+    | isYelling = "Whoa, chill out!"
+    | otherwise = "Whatever."
+    where isQuestion = last (dropWhileEnd isSpace xs) == '?'
+          letters = filter isLetter xs
+          isYelling = length letters /= 0 && all isUpper letters
