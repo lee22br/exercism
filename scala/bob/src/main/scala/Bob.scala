6a8a0e1caef20096fb2a455f7e75da630b0dd5d2
leandro.iglezias <leeiglezias@gmail.com>
2025-12-24 19:01:33 -0300

    Exercism Scala Bob

---
+++ scala/bob/src/main/scala/Bob.scala
@@ -0,0 +1,12 @@
+object Bob {
+  def response(statement: String): String =
+  val trimmed = statement.trim
+  val isSilent = trimmed.isEmpty
+  val isQuestion = trimmed.endsWith("?")
+  val isYelling = trimmed.exists(_.isLetter) && trimmed.forall(c => !c.isLetter || c.isUpper)
+  if isSilent then "Fine. Be that way!"
+  else if isQuestion && isYelling then "Calm down, I know what I'm doing!"
+  else if isYelling then "Whoa, chill out!"
+  else if isQuestion then "Sure."
+  else "Whatever."
+}
